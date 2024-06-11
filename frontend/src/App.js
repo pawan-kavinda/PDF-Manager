d2a921c4a15906f8b9c310848e122a969dbdb932
pawan-kavinda <138522618+pawan-kavinda@users.noreply.github.com>
2024-06-12 02:20:42 +0530

    routing_conditions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter,Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Registration from './components/authentication/Registration';
 import Login from './components/authentication/Login';
 import Navbar from './components/Navbar';
+import useAuthContext from './hooks/useAuthContext';
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <BrowserRouter>
     <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />        
-        <Route path="/signup" element={<Registration />} />
-        <Route path="/signin" element={<Login/>} />
+        <Route path="/" element={user?<Home />:<Navigate to='/signin'/>} />        
+        <Route path="/signup" element={!user?<Registration />: <Navigate to='/'/>} />
+        <Route path="/signin" element={!user?<Login/>:<Navigate to='/'/>} />
       </Routes>
     
     </BrowserRouter>
